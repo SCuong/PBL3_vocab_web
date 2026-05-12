0b01e15a101293b086f7f2382e7badb74166fb96
mercyk24 <102240398@sv1.dut.udn.vn>
2026-05-12 21:18:08 +0700

    Them note giong stick note

--- frontend/src/components/layout/index.ts
+++ frontend/src/components/layout/index.ts
@@ -2,3 +2,4 @@ export { Navbar } from './Navbar';
 export { Footer } from './Footer';
 export { UserWidget } from './UserWidget';
 export { AuthNavbar } from './AuthNavbar';
+export { StickyNotesWidget } from './StickyNotesWidget';
